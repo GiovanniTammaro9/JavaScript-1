592f9cbfa62fc8b98391efbcf332dda1943065e7
GiovanniTammaro9 <gio_play@live.it>
2024-11-07 13:43:54 +0100

    creata la variabile word

--- ciclo.js
+++ ciclo.js
@@ -2,6 +2,9 @@ let totale3 = 0;
 const LimiteDelFor = 20;
 
 for (let riga = 0; riga <= LimiteDelFor; riga = riga + 2) {
+    if(riga % 2 == 0) {
+        alert(riga);
+    }
     totale3 = totale3 + riga;
 }
 
