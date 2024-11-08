39df0a600f07d38b8801fab432ca9f916b29258e
GiovanniTammaro9 <gio_play@live.it>
2024-11-08 11:22:03 +0100

    fatto in modo che in console siano stampati i numeri pari

--- ciclo.js
+++ ciclo.js
@@ -3,7 +3,7 @@ const LimiteDelFor = 20;
 
 for (let riga = 0; riga <= LimiteDelFor; riga = riga + 2) {
     if(riga % 2 == 0) {
-        totale3 = totale3 + riga;
+        console.log(riga);
         
     }
     
